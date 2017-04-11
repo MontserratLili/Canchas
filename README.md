2a5bfc8df8d98ad030b5ec1de442dfa990ae0645
MontserratLili <lauraliligm@gmail.com>
2017-04-11 12:19:15 -0500

    cambios a canchas agregando funcionalidad

---
+++ README.md
@@ -0,0 +1,3 @@
+CANCHAS CON CSS
+
+El objetivo de este proyecto es realizar dibujos con CSS, en este caso, canchas deportivas de Basquet, Soccer y Tenis.
--- js/canchas.js
+++ js/canchas.js
@@ -1,1 +1,15 @@
+function mostrarSoccer(){
+document.getElementById("canchaSoccer").style.display = "none";
+}
 
+function mostrarBasquet(){
+document.getElementById("canchaBasquet").style.display = "none";
+}
+
+function mostrarTenis(){
+document.getElementById("canchaTenis").style.display = "none";
+}
+
+soccer.addEventListener("click", mostrarSoccer);
+basquet.addEventListener("click", mostrarBasquet);
+tenis.addEventListener("click", mostrarTenis);
