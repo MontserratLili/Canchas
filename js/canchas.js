22bfece06411bd2b134558336929e0fd48cf21f9
MontserratLili <lauraliligm@gmail.com>
2017-04-27 10:10:35 -0500

    logrando aparecer desaparecer canchas con js ejercicio terminado

--- js/canchas.js
+++ js/canchas.js
@@ -1,14 +1,42 @@
-function mostrarSoccer(){
-  document.getElementById("canchaSoccer").style.display = "block";
+function mostrarSoccer() {
+    document.getElementById("canchaSoccer").style.display = "block";
+    soccer.removeEventListener("click", mostrarSoccer);
+    soccer.addEventListener("click", ocultarSoccer);
+
 }
 
-function mostrarBasquet(){
-  document.getElementById("canchaBasquet").style.display = "block";
+function mostrarBasquet() {
+    document.getElementById("canchaBasquet").style.display = "block";
+    basquet.removeEventListener("click", mostrarBasquet);
+    basquet.addEventListener("click", ocultarBasquet);
 }
 
-function mostrarTenis(){
-  document.getElementById("canchaTenis").style.display = "block";
+function mostrarTenis() {
+    document.getElementById("canchaTenis").style.display = "block";
+    tenis.removeEventListener("click", mostrarTenis);
+    tenis.addEventListener("click", ocultarTenis);
 }
+
+
+
+function ocultarSoccer() {
+    document.getElementById("canchaSoccer").style.display = "none";
+    soccer.removeEventListener("click", ocultarSoccer);
+    soccer.addEventListener("click", mostrarSoccer);
+}
+
+function ocultarBasquet() {
+    document.getElementById("canchaBasquet").style.display = "none";
+    basquet.removeEventListener("click", ocultarBasquet);
+    basquet.addEventListener("click", mostrarBasquet);
+}
+
+function ocultarTenis() {
+    document.getElementById("canchaTenis").style.display = "none";
+    tenis.removeEventListener("click", ocultarTenis);
+    tenis.addEventListener("click", mostrarTenis);
+}
+
 
 soccer.addEventListener("click", mostrarSoccer);
 basquet.addEventListener("click", mostrarBasquet);
